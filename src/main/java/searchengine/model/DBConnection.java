359d9d4bb8062fe0d8de0d8d807f72bc563462f4
Daniil Anekov <daniilanekov@mail.ru>
2023-01-03 19:06:02 +0300

    In DBConnection added some methods.

--- src/main/java/searchengine/model/DBConnection.java
+++ src/main/java/searchengine/model/DBConnection.java
@@ -1,9 +1,16 @@
 package searchengine.model;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import searchengine.config.Site;
+import searchengine.config.SitesList;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class DBConnection {
     private static Connection connection;
     private static String dbName = "search_engine";
@@ -11,6 +18,8 @@ public class DBConnection {
     private static String dbPass = "galaxyfit";
     private static StringBuilder insertQuery = new StringBuilder();
     private static StringBuilder deleteQuery = new StringBuilder();
+    private static StringBuilder selectQuery = new StringBuilder();
+    private static SitesList sites;
 
     public static Connection getConnection() {
         if (connection == null) {
@@ -26,34 +35,51 @@ public class DBConnection {
     }
 
     public static void executeMultiInsert() throws SQLException{
-        //todo поправить String sql ниже.
-        String sql = "INSERT INTO voter_count(name, birthDate, `count`) " +
-                "VALUES " + insertQuery.toString() +
-                "ON DUPLICATE KEY UPDATE `count` = `count` + 1";
-        DBConnection.getConnection().createStatement().execute(sql);
+        for(Site site : sites.getSites()){
+            boolean isStart = insertQuery.length() == 0;
+            insertQuery.append((isStart ? "" :",") + "('" + site.getName() + "', '" + INDEXING + site.getUrl());//todo не верно написан sql запрос
+        }
+        String query = "INSERT INTO 'site'(name, status, url) " +
+                "VALUES " + insertQuery.toString();
+        DBConnection.getConnection().createStatement().execute(query);
         DBConnection.clearInsertQuery();
+        clearInsertQuery();
     }
 
-    public static void executeMultiDelete() throws SQLException{
-        String sql = "";
-        DBConnection.getConnection().createStatement().execute(sql);
-        DBConnection.clearDeleteQuery();
+    public static List<Integer> getSitesIdForDeletion() throws SQLException{
+        List<Integer> idList = new ArrayList<>();
+        for(Site site : sites.getSites()) {
+            selectQuery.append("SELECT 'id' FROM 'site' WHERE 'url' = " + site.getUrl() + "\n");
+        }
+        ResultSet rs = DBConnection.getConnection().createStatement().executeQuery(selectQuery.toString());
+        while (rs.next()) {
+            System.out.println("Id сайта на удаление - " + rs.getInt("id"));
+            idList.add(rs.getInt("id"));
+        }
+        clearSelectQuery();
+        return idList;
     }
+
+    public static void deleteInfoAboutSites () throws SQLException{
+        List<Integer> idList = getSitesIdForDeletion();
+        for(Integer id : idList) {
+            deleteQuery.append("DELETE FROM `site` WHERE 'id' = " + id + "DELETE FROM `page` WHERE 'site_id' = " + id);
+        }
+        DBConnection.getConnection().createStatement().execute(deleteQuery.toString());
+        DBConnection.clearDeleteQuery();
+        clearDeleteQuery();
+    }
+
     public static void clearInsertQuery(){
         insertQuery = new StringBuilder();
     }
-
     public static void clearDeleteQuery(){
         deleteQuery = new StringBuilder();
     }
-
-    public static StringBuilder getInsertQuery(){
-        return insertQuery;
+    public static void clearSelectQuery(){
+        selectQuery = new StringBuilder();
     }
 
-    public static StringBuilder getDeleteQuery(){
-        return deleteQuery;
-    }
 
 
 }
--- src/main/java/searchengine/services/IndexingService.java
+++ src/main/java/searchengine/services/IndexingService.java
@@ -4,11 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import searchengine.config.Site;
 import searchengine.config.SitesList;
+import searchengine.model.DBConnection;
 import searchengine.model.Page;
 import searchengine.model.PageLinksExtractor;
 
-import java.sql.*;
-import java.util.ArrayList;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
@@ -21,15 +21,13 @@ public class IndexingService {
     private final SitesList sites;
 
 
-    public void startIndexing(){
-        deleteInfoAboutSites();
+    public void startIndexing() throws SQLException {
+        DBConnection.deleteInfoAboutSites();
         String[] result = { "true", "false" };
         String error = "Данная страница находится за пределами сайтов,указанных в конфигурационном файле";
-        Set<String> allPages = new TreeSet<>();//TODO здесь все страницы со всех сайтов, указаных в конфиге .yaml. НО!!!! надо хранить этот сэт не здесь!!!! убрать! Оставить только старт
+        Set<String> allPages = new TreeSet<>();//TODO здесь будут все страницы со всех сайтов, указаных в конфиге .yaml. НО!!!! надо хранить этот сэт не здесь!!!! убрать! Оставить только старт
         List<Site> sitesList = sites.getSites();
         for(Site site : sitesList){
-
-
             new Thread(()->{
                 Page firstPage = new Page(site.getUrl());
                 PageLinksExtractor extractor = new PageLinksExtractor(firstPage);
@@ -39,46 +37,6 @@ public class IndexingService {
         }
     }
 
-    public void deleteInfoAboutSites () {
-        List<Integer> idList = getSitesIdForDeletion();
-        String deleteQuery = "";
-
-        for(Integer id : idList) {
-            deleteQuery += "DELETE FROM `site` WHERE 'id' = " + id + "DELETE FROM `page` WHERE 'site_id' = " + id;
-        }
-        System.out.println("Удаление из базы - " + statement.execute(deleteQuery)); //todo сделать класс подключения к базе!
-    }
-
-    public List<Integer> getSitesIdForDeletion(){
-        //todo перенести код про базу в класс DBConnections.
-        List<Integer> idList = new ArrayList<>();
-        String selectQuery = "";
-
-        for(Site site : sites.getSites()) {
-            selectQuery += "SELECT 'id' FROM 'site' WHERE 'url' = " + site.getUrl() + "\n";
-        }
-        String url = "jdbc:mysql://localhost:3306/search_engine";
-        String user = "root";
-        String pass = "galaxyfit";
-
-        try {
-            Connection connection = DriverManager.getConnection(url,user,pass);
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(selectQuery);
-            while (resultSet.next()){
-                int id = resultSet.getInt("id");
-                idList.add(id);
-                System.out.println(id);
-            }
-            resultSet.close();
-            statement.close();
-            connection.close();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
-        return idList;
-    }
     public void stopIndexing(){
         String[] result = { "true", "false" };
         String error = "Индексация не запущена";
